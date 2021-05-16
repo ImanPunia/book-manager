18dbc6e1e09adee35da7670786929fd568a475ef
Imandeep Kaur <imanpunia19@gmail.com>
2021-05-17 00:46:25 +1000

    BM: initial implementation of custom input
    added the wrapper for input form-element

---
+++ book-manager/src/app/form-elements/input/input/input.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    multi: true,
+    useExisting: InputComponent
+  }]
+})
+export class InputComponent implements ControlValueAccessor {
+
+  @Input() value:any;
+
+  onChange(value: any){ 
+    this.value = value;
+  }
+
+  constructor() { }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {  }
+
+
+}
