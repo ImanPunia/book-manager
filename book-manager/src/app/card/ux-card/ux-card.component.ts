c1b3618d9f65431c72cdf93d2595d5e71a96abdb
Imandeep Kaur <imanpunia19@gmail.com>
2021-06-19 19:34:25 +1000

    BM: blur the bottom of image

---
+++ book-manager/src/app/card/ux-card/ux-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ux-card',
+  templateUrl: './ux-card.component.html',
+  styleUrls: ['./ux-card.component.scss'],
+})
+export class UxCardComponent implements OnInit {
+  constructor() {}
+
+  value: string = 'djcjdfb';
+
+  ngOnInit(): void {}
+}
